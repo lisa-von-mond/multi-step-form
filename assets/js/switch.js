56ffa49ae7ae18eec9bc299b0e6a0af641121d8c
Lisa Lütkefedder <lisalutkefedder@SSQ-MBP14-M3-LLR.local>
2025-02-23 15:32:09 +0100

    add page indicators on sidebar to switch function

--- assets/js/switch.js
+++ assets/js/switch.js
@@ -1,6 +1,7 @@
 const nextButton = document.querySelectorAll('.button-next');
 const backButton = document.querySelectorAll('.button-back');
 
+/* button functions - WIP */
 nextButton.forEach(el => {
 	el.addEventListener('click', () => {
 		switchVisibility(true)
@@ -13,16 +14,27 @@ backButton.forEach(el => {
 	})
 })
 
+/* visibility switch function */
+
 function switchVisibility(next) {
 	const visibleSection = document.querySelector('section.active-section');
+	const currentIndicator = document.querySelector('.sidebar .active-section')
 	const nextSection = next === true ? visibleSection.nextElementSibling : visibleSection.previousElementSibling;
+	const nextIndicator = next === true ? currentIndicator.nextElementSibling : currentIndicator.previousElementSibling;
 	if (nextSection) {
-		visibleSection.classList.remove('active-section');
-		nextSection.classList.add('active-section');
+		if (nextIndicator) {
+			[visibleSection, currentIndicator, nextSection, nextIndicator].forEach(el => {
+				el.classList.toggle('active-section');
+			})
+		} else {
+			[visibleSection, nextSection].forEach(el => {
+				el.classList.toggle('active-section');
+			})
+		}
 	}
 }
 
-/* toggle switch */
+/* toggle switch on choose-plan section */
 const toggleSwitch = document.querySelector('.switch')
 const indicators = document.querySelectorAll('.indicator')
 const labelAttributes = document.querySelectorAll('.label-attribute')
