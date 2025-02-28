52e9964d60aa2dca5dfc5001f46227066fa737c0
Lisa Lütkefedder <lisalutkefedder@Mac.fritz.box>
2025-02-28 16:01:42 +0100

    refactor and shorten code

--- assets/js/summary.js
+++ assets/js/summary.js
@@ -1,16 +1,19 @@
 const planData = [
 	{
 		name: 'Arcade',
+		slug: 'arcade',
 		monthlyPrice: 9,
-		yearlyPrice: 0,
+		yearlyPrice: 90,
 	},
 	{
 		name: 'Advanced',
+		slug: 'advanced',
 		monthlyPrice: 12,
 		yearlyPrice: 120,
 	},
 	{
 		name: 'Pro',
+		slug: 'pro',
 		monthlyPrice: 15,
 		yearlyPrice: 150,
 	},
@@ -37,25 +40,11 @@ const extrasData = [
 	},
 ]
 
-const overviewBox = document.querySelector('.overview__box')
+const allData = [...planData, ...extrasData]
 
-function fillElements(planName, isYearly, planPrice) {
-	const overviewPlan = document.querySelector('.overview__plan')
-	const planNameEl = overviewPlan.querySelector('span:nth-child(1)')
-	const planPriceEl = overviewPlan.querySelector('span:nth-child(2)')
-	planNameEl.textContent = `${planName} (${isYearly === true ? 'yearly' : 'monthly'})`
-	planPriceEl.textContent = `${planPrice}$/${isYearly === true ? 'yr' : 'mo'}`
-}
-
-// function that will fill the price info dependent on if plan is yearly or monthly.
-function fillStaticElements(isYearly) {
-	const extrasEl = document.querySelectorAll('.overview__extra')
-	extrasEl.forEach(el => {
-		const extraSlug = el.dataset.extra
-		const extraPriceEl = el.querySelector('span:nth-child(2)')
-		const extrasPrice = extrasData.find(el => el.slug === extraSlug)[isYearly === true ? 'yearlyPrice' : 'monthlyPrice']
-		extraPriceEl.textContent = extrasPrice.toString()
-	})
+// helper function to convert price integer to info string
+function writePrice(int, isYearly) {
+	return `${int.toString()}$/${isYearly === true? 'yr' : 'mo'}`
 }
 
 // fill final summary with right data
@@ -64,18 +53,27 @@ planForm.addEventListener('change', () => {
 	const selectedPlan = planForm.querySelector('input[type=radio]:checked').getAttribute('value')
 	const isYearly = planForm.querySelector('input[type=checkbox]').checked
 	const price = planData.find(el => el.name === selectedPlan)[isYearly === true ? 'yearlyPrice' : 'monthlyPrice']
-	fillElements(selectedPlan, isYearly, price)
-	fillStaticElements(isYearly)
+	fillSummary(selectedPlan, isYearly, price)
+	fillPriceElements(isYearly)
 })
 
+// filling summary: upper part - name og plan, price of plan and interval - as well as total price (WIP)
+function fillSummary(planName, isYearly, planPrice) {
+	const overviewPlan = document.querySelector('.overview__plan')
+	const planNameEl = overviewPlan.querySelector('span:nth-child(1)')
+	const planPriceEl = overviewPlan.querySelector('span:nth-child(2)')
+	planNameEl.textContent = `${planName} (${isYearly === true ? 'yearly' : 'monthly'})`
+	planPriceEl.textContent = `${planPrice}$/${isYearly === true ? 'yr' : 'mo'}`
+}
 
 // change visibility of extras data in final summary
-const extrasFormInput = document.querySelectorAll('.choose-extras__form input')
 
+const extrasFormInput = document.querySelectorAll('.choose-extras__form input')
 extrasFormInput.forEach(el => {
 	el.addEventListener('change', () => {
 		const isChecked = el.checked
-		const wrapper = document.querySelector(`[data-extra="${el.getAttribute('value')}"]`);
+		const wrapper = document.querySelector(`[data-slug="${el.getAttribute('value')}"].overview__extra`);
+		console.log(wrapper)
 		if (isChecked) {
 			wrapper.classList.add('active')
 		} else {
@@ -83,3 +81,30 @@ extrasFormInput.forEach(el => {
 		}
 	})
 })
+
+
+// function that will fill the price info dependent on if plan is yearly or monthly.
+function fillPriceElements(isYearly) {
+	const planInputs = document.querySelectorAll('.choose-plan__label')
+	const extrasInputs = document.querySelectorAll('.choose-extras__label')
+	const extrasEl = document.querySelectorAll('.overview__extra')
+
+	function fill(el) {
+		el.forEach(el => {
+			const slug = el.dataset.slug
+			const priceEl = el.querySelector('.price-info')
+			const price = allData.find(el => el.slug === slug)[isYearly === true ? 'yearlyPrice' : 'monthlyPrice']
+			priceEl.textContent = writePrice(price, isYearly)
+		})
+	}
+
+	fill(planInputs);
+	fill(extrasInputs);
+	fill(extrasEl)
+}
+
+
+
+
+
+
