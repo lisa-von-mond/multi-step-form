02032a24fa39bf17f51e5aa25c9aab1b083c467d
Lisa Lütkefedder <lisalutkefedder@Mac.fritz.box>
2025-03-03 17:43:00 +0100

    change some principles of summary: summary is now willed on button click and not on input change anymore additional code that will calculate the final price

--- assets/js/summary.js
+++ assets/js/summary.js
@@ -42,48 +42,56 @@ const extrasData = [
 
 const allData = [...planData, ...extrasData]
 
-// helper function to convert price integer to info string
+// HELPER FUNCTION TO CONVERT PRICE INTEGER AND YEARLY/MONTHLY INFORMATION TO STRING EG '12$/mo'
 function writePrice(int, isYearly) {
 	return `${int.toString()}$/${isYearly === true? 'yr' : 'mo'}`
 }
 
-// fill final summary with right data
-const planForm = document.querySelector('.choose-plan__form')
-planForm.addEventListener('change', () => {
-	const selectedPlan = planForm.querySelector('input[type=radio]:checked').getAttribute('value')
-	const isYearly = planForm.querySelector('input[type=checkbox]').checked
-	const price = planData.find(el => el.name === selectedPlan)[isYearly === true ? 'yearlyPrice' : 'monthlyPrice']
-	fillSummary(selectedPlan, isYearly, price)
-	fillPriceElements(isYearly)
-})
 
-// filling summary: upper part - name og plan, price of plan and interval - as well as total price (WIP)
-function fillSummary(planName, isYearly, planPrice) {
-	const overviewPlan = document.querySelector('.overview__plan')
-	const planNameEl = overviewPlan.querySelector('span:nth-child(1)')
-	const planPriceEl = overviewPlan.querySelector('span:nth-child(2)')
-	planNameEl.textContent = `${planName} (${isYearly === true ? 'yearly' : 'monthly'})`
-	planPriceEl.textContent = `${planPrice}$/${isYearly === true ? 'yr' : 'mo'}`
-}
+// FILL FINAL OVERVIEW WITH RIGHT DATA
+const overviewButton = document.querySelector('#button-to-overview')
+overviewButton.addEventListener('click', fillOverview)
 
-// change visibility of extras data in final summary
+function fillOverview() {
 
-const extrasFormInput = document.querySelectorAll('.choose-extras__form input')
-extrasFormInput.forEach(el => {
-	el.addEventListener('change', () => {
+	// upper part - name of plan, price of plan and interval
+	const selectedPlan = document.querySelector('.choose-plan__form input[type=radio]:checked').getAttribute('value')
+	const isYearly = document.querySelector('.choose-plan__form input[type=checkbox]').checked
+
+	const planNameEl = document.querySelector('.overview__plan span:nth-child(1)')
+	const planPriceEl = document.querySelector('.overview__plan .price-info')
+
+	let finalPrice = 0
+
+	const planPrice = planData.find(el => el.name === selectedPlan)[isYearly === true ? 'yearlyPrice' : 'monthlyPrice']
+	finalPrice = planPrice
+	planNameEl.textContent = `${selectedPlan} (${isYearly === true ? 'yearly' : 'monthly'})`
+	planPriceEl.textContent = writePrice(planPrice, isYearly)
+
+	// change visibility of extras data in final summary
+	const extrasFormInput = document.querySelectorAll('.choose-extras__form input')
+	extrasFormInput.forEach(el => {
 		const isChecked = el.checked
 		const wrapper = document.querySelector(`[data-slug="${el.getAttribute('value')}"].overview__extra`);
-		console.log(wrapper)
+		const extrasPrice = extrasData.find(x => x.slug === el.getAttribute('value'))[isYearly === true ? 'yearlyPrice' : 'monthlyPrice']
 		if (isChecked) {
 			wrapper.classList.add('active')
+			finalPrice = finalPrice + extrasPrice
 		} else {
 			wrapper.classList.remove('active')
 		}
 	})
-})
 
+	// total line with final price
+	const summaryEl = document.querySelector('.overview__total span:nth-child(1)')
+	const finalPriceEl = document.querySelector('.overview__total span.price-info')
+	summaryEl.textContent = `Total (${isYearly === true ? 'per year' : 'per month'})`
+	finalPriceEl.textContent = writePrice(finalPrice, isYearly)
+}
 
-// function that will fill the price info dependent on if plan is yearly or monthly.
+// FILL ALL PRICE INFO WITH DATA DEPENDENT ON IF INTERVAL IS MONTHLY OR YEARLY
+
+// function that will fill all elements
 function fillPriceElements(isYearly) {
 	const planInputs = document.querySelectorAll('.choose-plan__label')
 	const extrasInputs = document.querySelectorAll('.choose-extras__label')
@@ -94,7 +102,7 @@ function fillPriceElements(isYearly) {
 			const slug = el.dataset.slug
 			const priceEl = el.querySelector('.price-info')
 			const price = allData.find(el => el.slug === slug)[isYearly === true ? 'yearlyPrice' : 'monthlyPrice']
-			priceEl.textContent = writePrice(price, isYearly)
+			priceEl.textContent = `+${writePrice(price, isYearly)}`
 		})
 	}
 
@@ -103,8 +111,12 @@ function fillPriceElements(isYearly) {
 	fill(extrasEl)
 }
 
+// call function once initially
+fillPriceElements(false)
 
-
-
-
-
+// call function whem toggle is switched
+const isYearlySwitch = document.querySelector('.switch')
+isYearlySwitch.addEventListener('click', function() {
+	const isYearly = event.target.checked
+	fillPriceElements(isYearly)
+})
